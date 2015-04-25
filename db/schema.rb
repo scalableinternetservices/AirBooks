f264ec54fe3bed0a29d0a85770609199a6c21616
Matt Agra <matt.agra@gmail.com>
2015-04-24 17:09:35 -0700

    add name, phone, location, to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150420060153) do
+ActiveRecord::Schema.define(version: 20150424235420) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 20150420060153) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name",                   limit: 255
+    t.string   "phone",                  limit: 255
+    t.string   "location",               limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
