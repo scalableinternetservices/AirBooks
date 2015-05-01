9f1d4cf1e276b35d96e6d4959706d46afa9aa333
neeravsharma <neerav@wifi-131-179-50-134.host.ucla.edu>
2015-05-01 03:17:41 -0700

    Update Book Index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,7 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all
+    @books = Book.where(owner_email: current_user.email)
   end
 
   # GET /books/1
