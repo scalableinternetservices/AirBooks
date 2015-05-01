6fbb3a7b954e5a89c16a09f7ecc4288eaeb74677
divakarbala <divakarbala@wifi-131-179-36-208.host.ucla.edu>
2015-05-01 15:18:39 -0700

    Updated old migration file for books

--- db/migrate/20150427054259_create_books.rb
+++ db/migrate/20150427054259_create_books.rb
@@ -1,6 +1,7 @@
 class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
+      t.string :book_id
       t.string :title
       t.string :author
       t.string :isbn
