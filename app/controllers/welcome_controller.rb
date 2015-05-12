859eb6dacc28662d8af6ea5b78357418d51ed806
Dongkeun Lee <ldkz2524@ucla.edu>
2015-05-12 05:08:17 -0700

    tiled image in welcome page & style fix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    #might not be scalable need to check
+    @books = Book.where.not(image_file_name: nil).order(:created_at).reverse_order.last(50).sample(20)
   end
 end
