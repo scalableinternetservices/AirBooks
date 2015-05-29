e33cf31e3e1732b346c2e03ecc2a06a773ec3931
Dongkeun Lee <ldkz2524@ucla.edu>
2015-05-28 23:01:09 -0700

    s3 implemented

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,16 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.paperclip_defaults = {
+      :storage => :s3,
+      :s3_credentials => {
+          :bucket => 'scalableinternetservices/AirBooks/',
+          :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+          :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      },
+      :s3_host_name => 's3-us-west-2.amazonaws.com'
+  }
 
 end
