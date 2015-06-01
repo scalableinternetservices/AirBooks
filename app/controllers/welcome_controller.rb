ba4e612b768457a5963ba51f3484c2945d8948a6
divakarbala <divakarbala@wifi-131-179-36-232.host.ucla.edu>
2015-05-31 21:03:09 -0700

    Added book counter

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,7 @@ class WelcomeController < ApplicationController
   def index
     #might not be scalable need to check
     @books = Book.where.not(image_file_name: nil).order(:created_at).reverse_order.last(50).sample(20)
+
+    @books_count = Book.count
   end
 end
