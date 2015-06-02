7db7a605360bf01f0335c0623ed6683213e20059
divakarbala <divakarbala@wifi-131-179-36-42.host.ucla.edu>
2015-06-02 15:19:37 -0700

    Made count more complex

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,7 @@ class WelcomeController < ApplicationController
     #might not be scalable need to check
     @books = Book.where.not(image_file_name: nil).order(:created_at).reverse_order.last(50).sample(20)
 
-    @books_count = Book.count
+    @books_count = Book.where("checked_out IS FALSE AND title LIKE ? OR author LIKE ? OR comments LIKE ?", "%92%", "%92%", "%92%").count
   end
 end
+	
