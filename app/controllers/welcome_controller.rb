6940337b37c2e492cae18af07894481dcb42a36e
neeravsharma <neerav@wifi-131-179-59-1.host.ucla.edu>
2015-06-02 16:07:28 -0700

    Added caching on welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,9 @@ class WelcomeController < ApplicationController
     #might not be scalable need to check
     @books = Book.where.not(image_file_name: nil).order(:created_at).reverse_order.last(50).sample(20)
 
-    @books_count = Book.where("checked_out IS FALSE AND title LIKE ? OR author LIKE ? OR comments LIKE ?", "%92%", "%92%", "%92%").count
+    @books_count = Rails.cache.fetch("books_count", expires_in: 1.hour) do
+    	Book.where("checked_out IS FALSE AND title LIKE ? OR author LIKE ? OR comments LIKE ?", "%92%", "%92%", "%92%").count
+    end
   end
 end
 	
