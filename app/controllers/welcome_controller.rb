32aa219e7d231eaad76afb1ea95681c1cce09bb0
neeravsharma <neerav@wifi-131-179-59-1.host.ucla.edu>
2015-06-02 19:58:50 -0700

    Add complex books count and caching

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,5 +10,8 @@ class WelcomeController < ApplicationController
     @books = search.results.sample(20)
 
     #@books = Book.where.not(image_file_name: nil).order(:created_at).reverse_order.last(50).sample(20)
+    @books_count = Rails.cache.fetch("books_count", expires_in: 1.hour) do
+    	Book.where("checked_out IS FALSE AND title LIKE ? OR author LIKE ? OR comments LIKE ?", "%92%", "%92%", "%92%").count
+    end
   end
 end
